5cd01903985e4ea7dc0a83ec3952cb025933ca2f
Simon Howard <fraggle@gmail.com>
2005-12-10 23:17:40 +0000

    Trie documentation

--- src/trie.h
+++ src/trie.h
@@ -33,7 +33,24 @@ POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-/* Trie: fast mapping of strings to values */
+/**
+ * @file trie.h
+ *
+ * @brief Fast string lookups
+ *
+ * A trie is a data structure which provides fast mappings from strings
+ * to values.
+ *
+ * To create a new trie, use @ref trie_new.  To destroy a trie,
+ * use @ref trie_free.
+ *
+ * To insert a value into a trie, use @ref trie_insert. To remove a value
+ * from a trie, use @ref trie_remove.  
+ *
+ * To look up a value from its key, use @ref trie_lookup.
+ *
+ * To find the number of entries in a trie, use @ref trie_num_entries.
+ */
 
 #ifndef ALGORITHM_TRIE_H
 #define ALGORITHM_TRIE_H
@@ -44,11 +61,59 @@ extern "C" {
 
 typedef struct _Trie Trie;
 
+/**
+ * Create a new trie.
+ *
+ * @return                   Pointer to a new trie structure.
+ */
+
 Trie *trie_new(void);
+
+/** 
+ * Destroy a trie.
+ *
+ * @param trie               The trie to destroy.
+ */
+
 void trie_free(Trie *trie);
+
+/**
+ * Insert a new key-value pair into a trie.
+ *
+ * @param trie               The trie.
+ * @param key                The key to access the new value.
+ * @param value              The value.
+ */
+
 void trie_insert(Trie *trie, char *key, void *value);
+
+/**
+ * Look up a value from its key in a trie.
+ *
+ * @param trie               The trie.
+ * @param key                The key.
+ * @return                   The value associated with the key, or NULL if
+ *                           not found in the trie.
+ */
+
 void *trie_lookup(Trie *trie, char *key);
+
+/**
+ * Remove an entry from a trie.
+ *
+ * @param trie               The trie.
+ * @param key                The key of the entry to remove.
+ */
+
 void trie_remove(Trie *trie, char *key);
+
+/** 
+ * Find the number of entries in a trie.
+ *
+ * @param trie               The trie.
+ * @return                   Count of the number of entries in the trie.
+ */
+
 int trie_num_entries(Trie *trie);
 
 #ifdef __cplusplus
