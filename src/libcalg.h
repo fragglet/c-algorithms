e8c8ef649865cb8629e331f8c6f361e6ffb04e43
Simon Howard <fraggle@gmail.com>
2005-12-20 23:20:23 +0000

    Main header

---
+++ src/libcalg.h
@@ -0,0 +1,59 @@
+
+/*
+ 
+Copyright (c) 2005, Simon Howard
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+
+ * Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in 
+   the documentation and/or other materials provided with the 
+   distribution.
+ * Neither the name of the C Algorithms project nor the names of its 
+   contributors may be used to endorse or promote products derived 
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+ 
+*/
+
+#ifndef LIBCALG_H
+#define LIBCALG_H
+
+#include <libcalg/compare-int.h>
+#include <libcalg/compare-pointer.h>
+#include <libcalg/compare-string.h>
+
+#include <libcalg/hash-int.h>
+#include <libcalg/hash-pointer.h>
+#include <libcalg/hash-string.h>
+
+#include <libcalg/arraylist.h>
+#include <libcalg/avltree.h>
+#include <libcalg/hashtable.h>
+#include <libcalg/list.h>
+#include <libcalg/queue.h>
+#include <libcalg/set.h>
+#include <libcalg/slist.h>
+#include <libcalg/trie.h>
+
+#endif /* #ifndef LIBCALG_H */
+
+
