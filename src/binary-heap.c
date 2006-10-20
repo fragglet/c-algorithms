9cad5c6248ec8eec8492a06bbecf56b19b7ad80e
Simon Howard <fraggle@gmail.com>
2006-10-20 23:34:08 +0000

    Add a binary heap implementation.

---
+++ src/binary-heap.c
@@ -0,0 +1,215 @@
+
+/*
+
+Copyright (c) 2005, Simon Howard
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+
+ * Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in 
+   the documentation and/or other materials provided with the 
+   distribution.
+ * Neither the name of the C Algorithms project nor the names of its 
+   contributors may be used to endorse or promote products derived 
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include <stdlib.h>
+
+#include "binary-heap.h"
+
+struct _BinaryHeap {
+        BinaryHeapType heap_type;
+	void **values;
+	int num_values;
+	int alloced_size;
+	BinaryHeapCompareFunc compare_func;
+};
+
+static int binary_heap_cmp(BinaryHeap *heap, void *data1, void *data2)
+{
+        if (heap->heap_type == BINARY_HEAP_TYPE_MIN) {
+                return heap->compare_func(data1, data2);
+        } else {
+                return -heap->compare_func(data1, data2);
+        }
+}
+
+BinaryHeap *binary_heap_new(BinaryHeapType heap_type,
+                            BinaryHeapCompareFunc compare_func)
+{
+	BinaryHeap *heap;
+
+	heap = malloc(sizeof(BinaryHeap));
+        heap->heap_type = heap_type;
+	heap->num_values = 0;
+	heap->compare_func = compare_func;
+	
+	/* Initial size of 16 elements */
+
+	heap->alloced_size = 16;
+	heap->values = malloc(sizeof(void *) * heap->alloced_size);
+	
+	return heap;
+}
+
+void binary_heap_insert(BinaryHeap *heap, void *value)
+{
+	int index;
+	int parent;
+
+	/* Possibly realloc the heap to a larger size */
+
+	if (heap->num_values >= heap->alloced_size) {
+
+		/* Double the table size */
+		
+		heap->alloced_size *= 2;
+		heap->values = realloc(heap->values, 
+		                       sizeof(void *) * heap->alloced_size);
+	}
+
+	/* Add to the bottom of the heap and start from there */
+
+	index = heap->num_values;
+	++heap->num_values;
+
+	/* Percolate the value up to the top of the heap */
+
+	while (index > 0) {
+
+		/* The parent index is found by halving the node index */
+
+		parent = index / 2;
+
+		/* Compare the node with its parent */
+
+		if (binary_heap_cmp(heap, heap->values[parent], value) < 0) {
+			
+			/* Ordered correctly - insertion is complete */
+
+			break;
+
+		} else {
+
+			/* Need to swap this node with its parent */
+
+			heap->values[index] = heap->values[parent];
+
+			/* Advance up to the parent */
+
+			index = parent;
+		}
+	}
+
+	/* Save the new value in the final location */
+
+	heap->values[index] = value;
+}
+
+void *binary_heap_pop(BinaryHeap *heap)
+{
+	void *result;
+	void *new_value;
+	int index;
+	int next_index;
+	int child1, child2;
+
+	/* Empty heap? */
+
+	if (heap->num_values == 0) {
+		return NULL;
+	}
+
+	/* Take the value from the top of the heap */
+
+	result = heap->values[0];
+
+	/* Remove the last value from the heap; we will percolate this down
+	 * from the top. */
+
+	new_value = heap->values[heap->num_values - 1];
+	--heap->num_values;
+
+	/* Percolate the new top value down */
+
+	index = 0;
+
+	for (;;) {
+
+		/* Calculate the array indexes of the children of this node */
+		
+		child1 = index * 2 + 1;
+		child2 = index * 2 + 2;
+
+		if (child1 < heap->num_values
+		 && binary_heap_cmp(heap, 
+                                    new_value, 
+                                    heap->values[child1]) > 0) {
+
+			/* Left child is less than the node.  We need to swap
+			 * with one of the children, whichever is less. */
+
+			if (child2 < heap->num_values
+			 && binary_heap_cmp(heap,
+                                            heap->values[child1],
+			                    heap->values[child2]) < 0) {
+				next_index = child1;
+			} else {
+				next_index = child2;
+			}
+			
+		} else if (child2 < heap->num_values
+		        && binary_heap_cmp(heap, 
+                                           new_value, 
+                                           heap->values[child2]) > 0) {
+
+			/* Right child is less than the node.  Swap with the 
+			 * right child. */
+
+			next_index = child2;
+
+		} else {
+			/* Node is less than both its children.  The heap condition
+			 * is satisfied.  We can stop percolating down. */
+
+			heap->values[index] = new_value;
+			break;
+		}
+
+		/* Swap the current node with the least of the child nodes. */
+
+		heap->values[index] = heap->values[next_index];
+
+		/* Advance to the child we chose */
+
+		index = next_index;
+	}
+
+    return result;
+}
+
+int binary_heap_num_entries(BinaryHeap *heap)
+{
+	return heap->num_values;
+}
+
---
+++ src/binary-heap.h
@@ -0,0 +1,138 @@
+
+/*
+
+Copyright (c) 2005, Simon Howard
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+
+ * Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in 
+   the documentation and/or other materials provided with the 
+   distribution.
+ * Neither the name of the C Algorithms project nor the names of its 
+   contributors may be used to endorse or promote products derived 
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/**
+ * @file binary-heap.h
+ *
+ * @brief Binary heap.
+ *
+ * A binary heap is a heap data structure implemented using a 
+ * binary tree.  In a heap, values are ordered by priority.
+ *
+ * To create a binary heap, use @ref binary_heap_new.  To destroy a 
+ * binary heap, use @ref binary_heap_free.
+ *
+ * To insert a value into a binary heap, use @ref binary_heap_insert.
+ *
+ * To remove the first value from a binary heap, use @ref binary_heap_pop.
+ *
+ */
+
+#ifndef ALGORITHM_BINARY_HEAP_H
+#define ALGORITHM_BINARY_HEAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Heap type.  If a heap is a min heap, the values with the lowest 
+ * priority are stored at the top of the heap and will be the first
+ * returned.  If a heap is a max heap, the values with the 
+ * greatest priority are stored at the top of the heap.
+ */
+
+typedef enum {
+    BINARY_HEAP_TYPE_MIN,
+    BINARY_HEAP_TYPE_MAX,
+} BinaryHeapType;
+
+/**
+ * Type of function used to compare values in a binary heap.
+ *
+ * @param data1            The first value.
+ * @param data2            The second value.
+ * @return                 A negative number if data1 is less than data2,
+ *                         a positive number if data1 is greater than data2,
+ *                         zero if the two are equal.
+ */
+
+typedef int (*BinaryHeapCompareFunc)(void *data1, void *data2);
+
+/** 
+ * A binary heap data structure.
+ */
+
+typedef struct _BinaryHeap BinaryHeap;
+
+/**
+ * Create a new @ref BinaryHeap. 
+ *
+ * @param heap_type        The type of heap: min heap or max heap.
+ * @param compare_func     Pointer to a function used to compare the priority
+ *                         of values in the heap.
+ * @return                 A new binary heap.
+ */
+
+BinaryHeap *binary_heap_new(BinaryHeapType heap_type,
+                            BinaryHeapCompareFunc compare_func);
+
+/**
+ * Destroy a binary heap.
+ *
+ * @param heap             The heap to destroy.
+ */
+
+void binary_heap_free(BinaryHeap *heap);
+
+/**
+ * Insert a value into a binary heap.
+ *
+ * @param heap             The heap to insert into.
+ * @param value            The value to insert.
+ */
+
+void binary_heap_insert(BinaryHeap *heap, void *value);
+
+/**
+ * Remove the first value from a binary heap.
+ *
+ * @param heap             The heap.
+ * @return                 The first value in the heap.
+ */
+
+void *binary_heap_pop(BinaryHeap *heap);
+
+/**
+ * Find the number of values stored in a binary heap.
+ *
+ * @param heap             The heap.
+ * @return                 The number of values in the heap.
+ */
+
+int binary_heap_num_entries(BinaryHeap *heap);
+
+#endif /* #ifndef ALGORITHM_BINARY_HEAP_H */
+
