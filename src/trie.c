fragglet/c-algorithms

commit 447e579062c76923751a884cdef73fe41ef06ba4

Use `next` field for freelist, not `data`
Since `TrieValue` might be replaced with a different type, we can't
assume that the `data` field can store a pointer.

src/trie.c
@@ -59,7 +59,7 @@ Trie *trie_new(void)
 
 static void trie_free_list_push(TrieNode **list, TrieNode *node)
 {
-	node->data = *list;
+	node->next[0] = *list;
 	*list = node;
 }
 
@@ -68,7 +68,7 @@ static TrieNode *trie_free_list_pop(TrieNode **list)
 	TrieNode *result;
 
 	result = *list;
-	*list = result->data;
+	*list = result->next[0];
 
 	return result;
 }

commit 57a7f68bb21d6c119bd7ef63646abdbbdd153ff5

Fix crash introduced by commit 447e579062c7692
It is not possible to use `next[0]` as the next pointer for the free
list since we use the `next[0]` when freeing nodes. However, we can
simplify the existing code significantly by ditching the free list
approach entirely and just using a recursive function to free the tree.

src/trie.c
@@ -60,53 +60,25 @@ Trie *trie_new(void)
 	return new_trie;
 }
 
-static void trie_free_list_push(TrieNode **list, TrieNode *node)
+static void free_node_recursive(TrieNode *node)
 {
-	node->next[0] = *list;
-	*list = node;
-}
+	int i;
 
-static TrieNode *trie_free_list_pop(TrieNode **list)
-{
-	TrieNode *result;
+	if (node == NULL) {
+		return;
+	}
 
-	result = *list;
-	*list = result->next[0];
+	/* Free all subnodes */
+	for (i = 0; i < 256; ++i) {
+		free_node_recursive(node->next[i]);
+	}
 
-	return result;
+	free(node);
 }
 
 void trie_free(Trie *trie)
 {
-	TrieNode *free_list;
-	TrieNode *node;
-	int i;
-
-	free_list = NULL;
-
-	/* Start with the root node */
-	if (trie->root_node != NULL) {
-		trie_free_list_push(&free_list, trie->root_node);
-	}
-
-	/* Go through the free list, freeing nodes.  We add new nodes as
-	 * we encounter them; in this way, all the nodes are freed
-	 * non-recursively. */
-	while (free_list != NULL) {
-		node = trie_free_list_pop(&free_list);
-
-		/* Add all children of this node to the free list */
-		for (i = 0; i < 256; ++i) {
-			if (node->next[i] != NULL) {
-				trie_free_list_push(&free_list, node->next[i]);
-			}
-		}
-
-		/* Free the node */
-		free(node);
-	}
-
-	/* Free the trie */
+	free_node_recursive(trie->root_node);
 	free(trie);
 }
 